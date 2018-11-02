4110eca60d1c7a0ec99424f5e2d874f9071c19a9
Jerrin Shaji George <jshajigeorge@vmware.com>
2018-11-02 10:46:24 -0700

    Revert "Throw error if compiled on targets other than linux amd64"
    This reverts commit 74001b664a7d716d163f7516c5c025cf4f1e6af7.

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -128,9 +128,6 @@ func xinit() {
 	if find(goos, okgoos) < 0 {
 		fatalf("unknown $GOOS %s", goos)
 	}
-	if goos != "linux" {
-		fatalf("target os should be linux")
-	}
 
 	b = os.Getenv("GOARM")
 	if b == "" {
@@ -181,9 +178,6 @@ func xinit() {
 	goarch = b
 	if find(goarch, okgoarch) < 0 {
 		fatalf("unknown $GOARCH %s", goarch)
-	}
-	if goarch != "amd64" {
-		fatalf("target architecture should be amd64")
 	}
 
 	b = os.Getenv("GO_EXTLINK_ENABLED")
